8bf53a6152c6da12419bd9cac15d0da5cb8d5394
Robin Drexler <drexler.robin@gmail.com>
2015-03-03 18:53:18 +0100

    try to get newly created pr url after creation through some awful redirect "following". next get title of page and send it to recents

--- extension/js/background.js
+++ extension/js/background.js
@@ -1,8 +1,22 @@
-chrome.webRequest.onBeforeRedirect.addListener(
-  function (details) {
-    console.log('request', details.redirectUrl);
-  },
-  {
+(function () {
+  var wasRedirected = false;
+
+  chrome.webRequest.onBeforeRedirect.addListener(function (details) {
+    // will be used in one of the next onCompleted listeners to extract the title
+    // and send it to the recents extension
+    wasRedirected = true;
+  }, {
     urls: ['*://github.com/*/pull/*']
-  }
-);
+  });
+
+  chrome.webRequest.onCompleted.addListener(function (details) {
+    if (wasRedirected) {
+      console.log('was redirected', details.tabId);
+
+      // unset so consecutive calls don't send the request to recents again
+      wasRedirected = false;
+    }
+  }, {
+    urls: ['*://github.com/*/pull/*']
+  });
+})();
